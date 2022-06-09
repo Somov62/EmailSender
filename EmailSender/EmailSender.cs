e29e3eef6284faf0ba00d61e1ef5be9c3a7ae7eb
olimp <olimp@MOBIL-OM>
2022-06-09 14:30:41 +0300

    Добавьте файлы проекта.

---
+++ EmailSender/EmailSender.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace EmailSender
+{
+    public class EmailSender
+    {
+        private async Task SendEmailAsync(MailMessage message)
+        {
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587)
+            {
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential("gostudymailsender@gmail.com", "daahodezlzbenqbo"),
+                EnableSsl = true
+            };
+            await smtp.SendMailAsync(message);
+        }
+
+        public void SimpleSend(string email, string subject, string messege,  string recipientName = "Someone")
+        {
+            MailAddress from = new MailAddress("gostudymailsender@gmail.com", "BOT");
+            MailAddress to = new MailAddress(email, recipientName);
+            MailMessage mail = new MailMessage(from, to)
+            {
+                Subject = subject,
+                Body = messege
+            };
+            Task.Factory.StartNew(() => SendEmailAsync(mail));
+        }
+
+    }
+}
---
+++ EmailSender/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmailSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmailSender sender = new EmailSender();
+            sender.SimpleSend("misha2003980@gmail.com", "aboba", "aboba");
+            Console.ReadKey();
+        }
+    }
+}
